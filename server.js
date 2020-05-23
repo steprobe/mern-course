e6b88a3ae57c73ec4ad6a4afb3e6acdb0e7de29e
Stephen roberts <stephen.roberts4@gmail.com>
2020-05-23 18:01:20 +0200

    MongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
