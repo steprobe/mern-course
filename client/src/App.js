68d6ad9adf029c248ffd68b1424bddcae095bfed
Stephen roberts <stephen.roberts4@gmail.com>
2020-05-24 17:54:51 +0200

    Front: Register + login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import NavBar from "./components/layout/NavBar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-const App = () =>
-  <Fragment>
-    <NavBar/>
-    <Landing/>
-  </Fragment>
+const App = () => (
+  <Router>
+    <Fragment>
+      <NavBar/>
+      <Route exact path="/" component={Landing}/>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
 export default App;
