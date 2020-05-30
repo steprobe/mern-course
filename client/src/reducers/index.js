e9c1d2d15d18a0ed860947bb83b455209c839349
Stephen roberts <stephen.roberts4@gmail.com>
2020-05-30 17:54:31 +0200

    More Lint

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
-import profile from './auth';
 
 export default combineReducers({
   alert,
-  auth,
+  auth
 });
