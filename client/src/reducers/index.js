fa273a473a58bb83b4bd5ea7e2318f6d21f44687
Stephen roberts <stephen.roberts4@gmail.com>
2020-05-30 17:00:04 +0200

    Profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers} from 'redux';
-import alert from './alert';
-import auth from './auth';
+import { combineReducers } from "redux";
+import alert from "./alert";
+import auth from "./auth";
+import profile from "./auth";
 
 export default combineReducers({
-  alert, auth
-})
+  alert,
+  auth,
+});
